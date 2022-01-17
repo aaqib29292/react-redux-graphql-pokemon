4dad81bb504b0c2d1318836ec50183e1d34d400e
Aaqib Parvez <aaqib29292@gmail.com>
2022-01-17 13:20:35 +0530

    changed api url

--- src/services/request.js
+++ src/services/request.js
@@ -1,7 +1,6 @@
 import { execute, makePromise , HttpLink } from 'apollo-boost';
- 
-const cors_api_url = 'https://cors-anywhere.herokuapp.com/';
-const uri = cors_api_url + 'https://graphql-pokemon2.vercel.app';
+
+const uri = 'https://graphql-pokemon2.vercel.app';
 const link = new HttpLink({ uri });
 
 const request = (query, variables) => {
